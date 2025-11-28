d00aa8e74b993dbd23550eb984c8827e111eb98a
Niki057-create <Nikithabalaji143@gmail.com>
2025-11-28 14:29:34 +0000

    Fix pytest import path for Jenkins

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,11 @@
+import sys, os
 import pytest
+
+# Add project root to PYTHONPATH for Jenkins
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app import app
+
 
 @pytest.fixture
 def client():
@@ -7,9 +13,8 @@ def client():
     with app.test_client() as client:
         yield client
 
+
 def test_home_page_status_code(client):
-    """
-    Simple test: home page (/) should return HTTP 200.
-    """
     response = client.get("/")
     assert response.status_code == 200
+
